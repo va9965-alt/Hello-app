2f4c51c027dae2d224d492af0cfe8d7fe239dd72
va9965-alt <va9965@srmist.edu.in>
2026-03-27 11:18:13 +0530

    UC3 COMPLETED

--- src/main/java/com/example/helloapp/HelloApp.java
+++ src/main/java/com/example/helloapp/HelloApp.java
@@ -1,6 +1,9 @@
 public class helloapp {
-	public static void main(string [] args) {
-		string name= args[0];
-		system.out.println("hello," + name + "!");
+	public static void main(string[] args) {
+		string name = "world";
+		if(args.lenth > 0 ) {
+			name =args[0];
+		}
+		System.out.println("hello, " + name + "!");
 	}
 }
