e1366462205beefae1840ed0b596efd9ef2779ce
va9965-alt <va9965@srmist.edu.in>
2026-03-27 11:35:58 +0530

    feature/UC4

--- src/main/java/com/example/helloapp/HelloApp.java
+++ src/main/java/com/example/helloapp/HelloApp.java
@@ -1,8 +1,15 @@
 public class helloapp {
 	public static void main(string[] args) {
-		string name = "world";
+		string name ="world";
 		if(args.lenth > 0 ) {
-			name =args[0];
+			StringBuilder nameBuilder = new StringBuilder();
+			for (int i = 0; i < args.lenth; i++) {
+				nameBuilder.append(args[i]);
+				if(i < args.length - 1) {
+					nameBuilder.append(",");
+				}
+			}
+			name=nameBuilder.toString();
 		}
 		System.out.println("hello, " + name + "!");
 	}
